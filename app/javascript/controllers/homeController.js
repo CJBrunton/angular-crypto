47113e1e6d7f63dba0b383606cc242754af99701
Chris Brunton <chris@appointedd.com>
2018-03-07 16:40:32 +0000

    Add gitignore file and a loader for whilst fetching all currencies

--- app/javascript/controllers/homeController.js
+++ app/javascript/controllers/homeController.js
@@ -1,9 +1,20 @@
 mainApp.controller("homeCtrl", ['$scope', '$http', function($scope, $http){	
+
+	/*
+	* ui elements rendered on the view
+	*/
+	$scope.ui = {
+		isLoading:true
+	}
+
+	/*
+	* Call to coin market cap to return all crypto currencies
+	*/
 	$http.get(" https://api.coinmarketcap.com/v1/ticker/")
     .then(function(response) {
-        $scope.currencies = response.data;
-        console.log('derpsss',$scope.currencies)
+		$scope.ui.isLoading = false;    	
+        $scope.currencies = response.data;        
     }, function(error){
-    	console.log('derp error derps');	
+    	console.log('error from call to coinmarket cap', error);	
     })
 }]);
