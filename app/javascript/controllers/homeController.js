abde5dd5030d08a547bcadd243834ef7f7259cf8
Chris Brunton <chris@appointedd.com>
2019-06-21 15:55:17 +0100

    Add eslint and prettier

--- app/javascript/controllers/homeController.js
+++ app/javascript/controllers/homeController.js
@@ -1,20 +1,25 @@
-mainApp.controller("homeCtrl", ['$scope', '$http', function($scope, $http){	
+angular.module("mainApp").controller("HomeController", [
+	"$scope",
+	"$http",
+	function($scope, $http) {
+		/*
+		 * ui elements rendered on the view
+		 */
+		$scope.ui = {
+			isLoading: true
+		};
 
-	/*
-	* ui elements rendered on the view
-	*/
-	$scope.ui = {
-		isLoading:true
+		/*
+		 * Call to coin market cap to return all crypto currencies
+		 */
+		$http.get(" https://api.coinmarketcap.com/v1/ticker/").then(
+			function(response) {
+				$scope.ui.isLoading = false;
+				$scope.currencies = response.data;
+			},
+			function(error) {
+				console.log("error from call to coinmarket cap", error);
+			}
+		);
 	}
-
-	/*
-	* Call to coin market cap to return all crypto currencies
-	*/
-	$http.get(" https://api.coinmarketcap.com/v1/ticker/")
-    .then(function(response) {
-		$scope.ui.isLoading = false;    	
-        $scope.currencies = response.data;        
-    }, function(error){
-    	console.log('error from call to coinmarket cap', error);	
-    })
-}]);
+]);
